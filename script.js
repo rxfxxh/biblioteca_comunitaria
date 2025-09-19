e7344de6c1b1faaee06f016351e1ca9e8bc92a48
Rafael <160151456+rxfxxh@users.noreply.github.com>
2025-09-19 12:22:23 -0300

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("myButton").addEventListener("click", function() {
+        console.warn("Samuel da tribo indigena");
+    });
+});
