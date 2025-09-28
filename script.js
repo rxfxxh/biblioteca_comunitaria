19f41f5cd1afe8739937247182197adb829d309b
Rafael <160151456+rxfxxh@users.noreply.github.com>
2025-09-27 23:07:02 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Garante que o DOM está carregado antes de adicionar o evento
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("myButton").addEventListener("click", function () {
-        alert("Samuel da tribo indigena");
-    });
-});
